3e709bc1870fe936cf7351676c06b0b122189f7e
alberto139 <alberto.rivera139@gmail.com>
2016-04-13 15:44:37 -0400

    Finished P0

--- MLND/wordcount.py
+++ MLND/wordcount.py
@@ -22,14 +22,16 @@ def count_words(s, n):
 
     # TODO: Return the top n words as a list of tuples (<word>, <count>)
     #return top_n
+    print len(l)
     return l[0:n]
 
 
 
 def test_run():
     """Test count_words() with some inputs."""
-    print count_words("cat bat mat cat bat cat", 3)
-    print count_words("betty bought a bit of butter but the butter was bitter", 3)
+   # print count_words("cat bat mat cat bat cat", 3)
+   # print count_words("betty bought a bit of butter but the butter was bitter", 3)
+    print count_words("offer is secret click secret link secret sports link play sports today secret sports event sport is today sport cost money", 15)
 
 
 if __name__ == '__main__':
